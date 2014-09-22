766ab28a2dd183c96288f69d87d036e18cc79ea1
mcoenca <m.coenca@gmail.com>
2014-09-22 12:37:07 -0700

    mvp one working

--- tilt.js
+++ tilt.js
@@ -1,23 +1,38 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+Topics = new Mongo.Collection("topics");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+if (Meteor.isClient) {
+  Template.topicboard.topics = function () {
+    return Topics.find({}, {sort: {score: -1, name: 1}});
+  };
+
+  Template.topic.selected = function () {
+    return Session.equals("selected_topic", this._id) ? "selected" : '';
+  };
+
+  Template.topic.events({
+    'click': function () {
+      Session.set("selected_topic", this._id);
+      Topics.update(this._id, {$inc: {score: 1}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.topicboard.events({
+    'click button' : function(){
+      var input = Template.instance().$("input");
+      Topics.insert({name: input.val(), score: 0});
+      input.val('');
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (Topics.find().count() === 0) {
+      var names = ["I am bored :(",
+                   "I want to eat something"];
+      for (var i = 0; i < names.length; i++)
+        Topics.insert({name: names[i],
+                        score: 0});
+    }
   });
 }
