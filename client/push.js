bbf94d40a1f06300c915be266853f5fe97b47ae0
mcoenca <m.coenca@gmail.com>
2014-09-28 22:46:09 -0700

    refactored push to publish the device token

--- client/push.js
+++ client/push.js
@@ -1,36 +1,46 @@
-Meteor.startup(function () {
-  if (Meteor.isCordova){
-    var device_token = '';
+  var pushStuff = function () {
+
     var pushNotification = window.plugins.pushNotification;
     //pour le push
     var pushSuccessHandler = function (result) { 
-      alert('result = ' + result); 
-    };
-    var pushErrorHandler = function (error) { 
-      alert('error = ' + error); 
-    };
-    var pushTokenHandler = function (result) { 
-      console.log('iOS device token = ' + result); 
-      device_token=result;
+      alert('pushResult = ' + result); 
     };
 
-    pushNotification.register(
-      pushTokenHandler,
-      pushErrorHandler,
-      {"badge":"true",
-      "sound":"true",
-      "alert":"true",
-      "ecb":"onNotificationAPN"});
-    Deps.autorun(function(){ 
-      if(Meteor.userId() && device_token !== ''){
-        console.log('hey');
+    var pushErrorHandler = function (error) { 
+      alert('pushError = ' + error); 
+    };
+
+    var pushTokenHandler = function (result) { 
+      console.log('iOS device token = ' + result); 
+      Meteor.user().setDevice(result);
+    };
+
+    var myself = {
+
+      registerForNotifications:function (){
+        pushNotification.register(
+          pushTokenHandler,
+          pushErrorHandler,
+          {
+          "badge":"true",
+          "sound":"true",
+          "alert":"true",
+          "ecb":"onNotificationAPN"
+          }
+        );
       }
-    });     
-  } 
+    };
+
+    return myself;
+
+  }.call();
+
+Meteor.startup(function () {
+  if (Meteor.isCordova){
     Deps.autorun(function(){ 
-      if(Meteor.user()){
-        console.log(Meteor.userId());
-        console.log(Meteor.user().uname());
+      if(Meteor.userId()){
+        pushStuff.registerForNotifications();
       }
-    });     
+    });
+  }   
 });
