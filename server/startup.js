4abee2ee63934fecc26c0b328fb4cb798b6f15a3
mcoenca <m.coenca@gmail.com>
2014-09-29 16:52:10 -0700

    PRODUCTION TEST

--- server/startup.js
+++ server/startup.js
@@ -8,7 +8,8 @@ Meteor.startup(function () {
   var apnConnection;
 
   // default apn connection options
-  var cert = Assets.getText("cert.pem");
+  // var cert = Assets.getText("cert.pem");
+  var cert = Assets.getText("tiltProdCert.pem");
   var key = Assets.getText("key.pem");
   // console.log(cert);
   // console.log(key);
@@ -16,21 +17,22 @@ Meteor.startup(function () {
     {
     cert: cert,
     key: key,
-    passphrase: 'cristohoger24'
+    passphrase: 'cristohoger24',
+    production: true
     }, 
     apnOptions);
   // console.log(apnOptions);
 
   apnConnection = new apn.Connection(apnOptions);
 
-  var sendAppleNotifications = function (topic_id, content, url) {
+  var sendAppleNotifications = function (topic_id, content) {
     var note = new apn.Notification();
     // expires 1 hour from now
     note.expiry = Math.floor(Date.now() / 1000) + 3600;
     note.badge = 1;
     note.sound = alertSound;
     note.alert = content;
-    note.payload = {'url': url};
+    note.payload = {'url': "/topics/"+topic_id};
 
     var topic = Topics.findOne(topic_id);
     var pushIds = topic.userDeviceTokens();
@@ -46,7 +48,7 @@ Meteor.startup(function () {
   //Declare the methods on the server that can be accessed by the client
   Meteor.methods({
     sendNotificationsToTopicUsers: function(args) {
-      sendAppleNotifications(args[0],args[1],"heyhey");
+      sendAppleNotifications(args[0],args[1]);
     }
   });
 });
