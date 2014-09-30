6fafea56f05992d60b41afe87a55f49aff5328af
mcoenca <m.coenca@gmail.com>
2014-09-29 22:05:08 -0700

    a lot of bug fixes in order to make the user profile working even without facebook, especially creating a empty profile on creation

--- server/startup.js
+++ server/startup.js
@@ -1,5 +1,15 @@
 
 Meteor.startup(function () {
+
+  Accounts.onCreateUser(function(options, user) {
+    // We're enforcing at least an empty profile object to avoid needing to check
+    // for its existence later.
+    user.profile = options.profile ? options.profile : {};
+    user.profile.username = user.emails[0].address.split("@",1)[0];
+    console.log(user);
+    return user;
+  });
+
   //Enable push notification by adding the relevant package
   var apn = Meteor.npmRequire("apn");
   var path = Npm.require('path');
