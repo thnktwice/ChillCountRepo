ccf7932037901e06fadec6af3312c8fbb1be09d2
mcoenca <m.coenca@gmail.com>
2014-10-03 19:04:18 -0700

    added node gcm package

--- cordova-build-override/plugins/com.phonegap.plugins.PushPlugin/Example/server/pushGCM.rb
+++ cordova-build-override/plugins/com.phonegap.plugins.PushPlugin/Example/server/pushGCM.rb
@@ -4,6 +4,6 @@ GCM.host = 'https://android.googleapis.com/gcm/send'
 GCM.format = :json
 GCM.key = "AIzaSyCZL1dvqOyyRrULR_hjACsWQKQlZrdqO4s"
 destination = ["APA91bFeozxm32ZgOmv5JhLDxLjRS-pD5-nYpRQqsfAOBoetXHzBrX0rX7REFw4ltm6R5Aj8S8Tu24vzhP9rfiEAFuPdMLPjkRUCv5yunFlxV9xfyAZJwfd5zZxrzjAfbPqFtDBLwpTl-2UaGYrmwulvSGFnAQCJ0A"]
-data = {:message => "PhoneGap Build rocks!", :msgcnt => "1", :soundname => "beep.wav"}
+data = {:message => "PhoneGap Builds rocks!", :msgcnt => "1", :soundname => "beep.wav"}
 
 GCM.send_notification( destination, data)
