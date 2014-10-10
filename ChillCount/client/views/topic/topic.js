f136726874ed762c3472fc4d7370c75e8d6fb163
mcoenca <m.coenca@gmail.com>
2014-10-09 22:31:41 -0700

    broke down all the client (not mobile) javascript into separatate files relevant next to their related html template views

---
+++ ChillCount/client/views/topic/topic.js
@@ -0,0 +1,37 @@
+Template.topic.selected = function () {
+  return Session.equals("selected_topic_id", this._id) ? "selected" : '';
+};
+
+Template.topic.helpers({
+  currentUserIsAdmin: function() {
+    if (Meteor.user()) {
+      return Meteor.user().isAdmin();      
+    }
+    else {
+      return false;
+    }
+  },
+  beanIsConnected: function (){
+    return Session.equals('bluetooth_status','bean_connected');
+  }
+});
+
+Template.count.isMine = function () {
+  return (this.user_id === Meteor.userId()) ;
+};
+
+Template.message.isMine = function () {
+  return (this.user_id === Meteor.userId()) ;
+};
+
+Template.topic.events({
+  'click .plus': function () {
+    this.addACount(Meteor.userId());
+  },  
+  'click .go': function () {
+    Router.go("/topics/"+this._id);
+  },
+  'click .link_icon': function (){
+    Session.set("selected_topic_id", this._id);
+  }
+});
