d2d6052f4be44663e419ca2bf7c9b0132e8ed0f2
mcoenca <m.coenca@gmail.com>
2014-10-13 16:47:54 -0700

    added defaults values for testing on meteor server startup

--- ChillCount/server/startup/defaults.js
+++ ChillCount/server/startup/defaults.js
@@ -9,4 +9,43 @@ Meteor.startup(function () {
     return user;
   });
 
+  //Defaults values for users and topics on startup
+  //for testing purposes
+  if (Meteor.users.find().count() === 0) {
+    var user_id = Accounts.createUser({
+      email:"m.coenca@gmail.com",
+      password:"cristohoger24"
+    });
+    var topicMore_id = Topics.insert({
+      user_id:user_id,
+      name:"TestMore",
+      description:"TestMore Description",
+      type: "private"
+    });    
+    var topicLess_id = Topics.insert({
+      user_id:user_id,
+      name:"TestLess",
+      description:"TestLess Description",
+      type:"private"
+    });
+    var topicMore = Topics.findOne(topicMore_id);
+    var topicLess = Topics.findOne(topicLess_id);
+    topicMore.addACount(user_id);
+    topicMore.addACount(user_id);
+    topicMore.addACount(user_id);
+    topicLess.addACount(user_id);
+    var goalMore_id = DailyGoals.insert({
+      topic_id:topicMore_id,
+      user_id:user_id,
+      comparator:"moreThan",
+      value:5
+    });
+
+    var goalLess_id = DailyGoals.insert({
+      topic_id: topicLess_id,
+      user_id:user_id,
+      comparator:"lessThan",
+      value:8
+    });
+  }
 });
