af5323fdf158fbe71cf5f508c4e9878812621f76
mcoenca <m.coenca@gmail.com>
2014-10-13 01:23:46 -0700

    added db schema through the collection2 plugin. That provides a simple way to define what each collection should look like and ensure a validation server side upon database modifiction

---
+++ ChillCount/db/schema.js
@@ -0,0 +1,57 @@
+var Schemas = {};
+
+//The schema for users is a next step because it is managed through
+//Meteor.users and not really stable depending on the login options
+Schemas.Log = new SimpleSchema({
+    topic_id: {
+        type: String,
+        label: "Topic"
+    },
+    user_id: {
+        type: String,
+        label: "User"
+    },
+    timestamp: {
+        type: Number,
+        label: "Timestamp"
+    },
+    type: {
+        type: String,
+        label: "Log type : count, message, adminMessage ",
+    },
+    content: {
+        type: String,
+        label: "Content of the message",
+        optional: true,
+        max: 1000
+    }
+});
+
+Schemas.Topic = new SimpleSchema({
+    user_id: {
+        type: String,
+        label: "User"
+    },
+    name: {
+        type: String,
+        label: "topic Name"
+    },
+    type: {
+        type: String,
+        label:"Topic type : private or public"
+    },
+    description: {
+        type: String,
+        label: "Topic Description",
+        optional: true,
+        max: 1000
+    },
+    score: {
+        type: Number,
+        label: "Number of counts in the topic",
+        min: 0
+    }
+});
+
+Logs.attachSchema(Schemas.Log);
+Topics.attachSchema(Schemas.Topic);
